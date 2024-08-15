db7bea94216e41f1011c246cf2ca18d085b06967
NouranElsyed <es-nournelsayed54@alexu.edu.eg>
2024-08-15 11:08:22 +0300

    Restriction Operators

---
+++ Assignment/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Specialized;
+using System.Runtime.Versioning;
+using static Assignment.ListGenerators;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  LINQ - Restriction Operators
+            #region Q1
+            //1. Find all products that are out of stock.
+            //var OutOfStock = ProductList.Where(P => P.UnitsInStock == 0);
+            //foreach (var Product in OutOfStock)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+            #endregion
+            #region Q2
+            //2. Find all products that are in stock and cost more than 3.00 per unit.
+            //var ProuductInStockCostMoreThan3 = from P in ProductList
+            //                                   where P.UnitsInStock>0 && P.UnitPrice>3
+            //                                   select P;
+            //foreach (var Product in ProuductInStockCostMoreThan3)
+            //{
+            //    Console.WriteLine(Product);
+            //}
+            #endregion
+            #region Q3 
+            //3. Returns digits whose name is shorter than their value.
+            //String[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            //var DigitsNameShorterThnTheirValue = Arr.Where((D,I)=>D.Length< I );
+            //foreach (var digit in DigitsNameShorterThnTheirValue) 
+            //{
+            //    Console.WriteLine(digit);
+            //}
+            #endregion
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+
+            #endregion
+            #region
+
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+            #region
+            #endregion
+        }
+    }
+}
